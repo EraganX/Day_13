025c4f43079be19bfe2c21051abca4452ef71e75
Charith Eranga <Charithccen@gmail.com>
2024-08-05 23:42:10 +0530

    enemy health bar

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float health = 50f;
+    private float currentHealth = 0;
     public int coin = 100;
     public Transform target; 
+
 
     private NavMeshAgent agent;
 
 
+    public Canvas canvas;
+    public Image healthBar;
+
+
     private void Awake()
     {
+        canvas.worldCamera = Camera.main;
         target = GameObject.FindGameObjectWithTag("End").transform;
+
+        currentHealth = health;
+
+        healthBar.fillAmount = currentHealth / health;
     }
 
     void Start()
@@ -36,8 +48,11 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
-        health -= amount;
-        if (health <= 0f)
+        currentHealth -= amount;
+
+        healthBar.fillAmount = currentHealth / health;
+
+        if (currentHealth <= 0f)
         {
             GameManager.Instance.money += coin;
             GameManager.Instance.kills += 1;
